e7ec06a4a17b356be0cd3bc888c99605682d55f7
Julian Skupnjak <julian.skupnjak@gmail.com>
2018-06-27 18:30:17 +0200

    update script

--- deploy-eirini/run.sh
+++ deploy-eirini/run.sh
@@ -21,7 +21,7 @@ clone_repos() {
 }
 
 set_env() {
-  export BOSH_DEPLOYMENT=${BOSH_DEPLOYMENT:-"$EIRINI_LITE/bosh-deployment"}
+  export BOSH_DEPLOYMENT_DIR=${BOSH_DEPLOYMENT:-"$EIRINI_LITE/bosh-deployment"}
 	export CF_DEPLOYMENT=${CF_DEPLOYMENT:-"$EIRINI_LITE/cf-deployment"}
 	export EIRINI_RELEASE=${EIRINI_RELEASE:-"$EIRINI_LITE/eirini-release"}
 	export CAPI_RELEASE=${CAPI_RELEASE:-"$EIRINI_LITE/capi-release"}
