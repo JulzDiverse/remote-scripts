0ca28d78832c0d61b35795c456f7e44ce28d06b7
Julian Skupnjak <julian.skupnjak@gmail.com>
2018-07-05 18:16:08 +0200

    update

--- eirini-check/run.sh
+++ eirini-check/run.sh
@@ -44,7 +44,7 @@ check_kubectl(){
 }
 
 check_ruby(){
-  ruby --version
+	ruby --version | grep 2.4
 	local error_code=$?
 	message_on_error $error_code "ruby not installed or not in path"
 	message_on_success $error_code ":::: OK -> just make sure you have ruby 2.4 installed"
